342a64e2b41a4bd265b083cf1a9b824e6975a013
NathanJeong <dh.jeong@idoo.kr>
2022-01-24 23:27:33 +0900

    #2.1 Test Drive Part 1
    learn how to use Tailwind CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,28 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-blue-500">It works!</h1>
+    <div className="bg-slate-400 py-20 px-10 flex flex-col space-y-5">
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
+        <span className="font-bold text-2xl">Selet Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span className="font-semibold">$170</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Table</span>
+          <span className="font-semibold">$800</span>
+        </div>
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
+          <span>Total</span>
+          <span>$970</span>
+        </div>
+        <div className="mt-5 bg-sky-500 text-white text-center p-2 rounded-xl w-1/2 mx-auto">
+          Check Out
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
   );
 };
